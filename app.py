a84883d8513c81b7bf9bb5a01b7b603f58d9f286
yamami0301 <allstar.naikaku6@gmail.com>
2020-09-03 17:56:04 +0900

    fizzbuzz完了

---
+++ app.py
@@ -0,0 +1,4 @@
+from fizzbuzz import fizzbuzz_convert
+
+for number in range(1, 101):
+    print(fizzbuzz_convert(number))
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,14 @@
 def fizzbuzz_convert(number):
-    for number in range(1, 101):
+    if number % 15 == 0:
+        return 'FizzBuzz'
+    if number % 5 == 0:
+        return 'buzz'
+    if number % 3 == 0:
+        return 'fizz'
+    return str(number)
 
-        if number % 15 == 0:
-            return 'fizzbuzz'
-        if number % 5 == 0:
-            return 'buzz'
-        if number % 3 == 0:
-            return 'fizz'
-        else:
-            return number
 
-    assert fizzbuzz_convert(15) == 'fizzbuzz'
-    assert fizzbuzz_convert(5) == 'buzz'
-    assert fizzbuzz_convert(3) == 'Fizz'
-    assert fizzbuzz_convert(number) == number
+assert fizzbuzz_convert(15) == 'FizzBuzz'
+assert fizzbuzz_convert(5) == 'buzz'
+assert fizzbuzz_convert(3) == 'fizz'
+assert fizzbuzz_convert(1) == '1'
